4057a6ad9f05a93b287ed972929eda48f3068a87
Goh Sheen An <goh_sheen_an@tech.gov.sg>
2022-06-07 22:40:19 +0800

    Add Logging

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Patch, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Get, HttpCode, Logger, Param, ParseIntPipe, Patch, Post} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Like, MoreThan, Repository} from "typeorm";
 import {Event} from '../events/event.entity';
@@ -7,14 +7,19 @@ import {UpdateEventDto} from "./update-event.dto";
 
 @Controller('/events')
 export class EventsController {
+  private readonly logger = new Logger(EventsController.name);
+
   constructor (
     @InjectRepository(Event)
     private readonly repository: Repository<Event>
   ) {}
 
   @Get()
-  findAll () {
-    return this.repository.find();
+  async findAll () {
+    this.logger.log(`Hit the findAll route`);
+    const events = await this.repository.find();
+    this.logger.debug(`Found ${ events.length } events`);
+    return events;
   }
 
   @Get('/practice')
